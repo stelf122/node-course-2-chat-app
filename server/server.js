8c1f5f1258e4a6956a43cbbd59670884175d068d
Anton <vasilevsky_anton@mail.ru>
2018-11-28 15:13:56 +0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,18 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'See you then',
-        createdAt: 123123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-    });
-
-    socket.on('createEmail', (newEmail) => {
-        console.log('createEmail', newEmail);
+        io.emit('newMessage', {
+            from: message.from,
+            test: message.text,
+            createdAt: new Date().getTime()
+        })
     });
     
     socket.on('disconnect', () => {
